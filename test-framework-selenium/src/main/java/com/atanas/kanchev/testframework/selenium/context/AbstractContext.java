a25e98fb25ed628d9d1fc14407d960181cbba4d5
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-03-30 17:33:09 +0100

    AbstractContext is now generic

--- test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/context/AbstractContext.java
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/context/AbstractContext.java
@@ -3,5 +3,11 @@ package com.atanas.kanchev.testframework.selenium.context;
 /**
  * Created by atanas on 24/03/2016.
  */
-public class AbstractContext {
+public abstract class AbstractContext<T> extends ContextFactory {
+
+    T driver;
+
+    protected abstract T getDriver();
+
+    protected abstract void setDriver(T driver);
 }
