9f27b61d0fe3cc3dff5696942f7273e7bc470239
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-03-30 17:34:34 +0100

    Driver object moved to superclass

--- test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/context/WebContext.java
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/context/WebContext.java
@@ -8,10 +8,7 @@ import java.util.List;
 /**
  * Created by atanas on 24/03/2016.
  */
-public final class WebContext extends AbstractContext {
-
-    // Driver object
-    private WebDriver driver;
+public final class WebContext extends AbstractContext<WebDriver> {
 
     // Current WebElement pointer
     private WebElement currentElement;
@@ -25,8 +22,8 @@ public final class WebContext extends AbstractContext {
 
     public WebDriver getDriver() {
 
-        if (driver == null) throw new RuntimeException("Null driver");
-        else return driver;
+        if (super.driver == null) throw new RuntimeException("Null driver");
+        else return super.driver;
     }
 
     public WebElement getCurrentElement() {
@@ -42,7 +39,7 @@ public final class WebContext extends AbstractContext {
     /////////////
 
     public void setDriver(WebDriver driver) {
-        this.driver = driver;
+        super.driver = driver;
     }
 
     public void setCurrentElement(WebElement currentElement) {
@@ -51,5 +48,10 @@ public final class WebContext extends AbstractContext {
 
     public void setWebElementsList(List<WebElement> webElementsList) {
         this.webElementsList = webElementsList;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getName();
     }
 }
