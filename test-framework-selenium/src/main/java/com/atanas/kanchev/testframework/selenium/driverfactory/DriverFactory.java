6f868abce4112e72d858bf03cab9a30a75bf0056
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-03-30 17:35:12 +0100

    Added setter for the driver object

--- test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/driverfactory/DriverFactory.java
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/driverfactory/DriverFactory.java
@@ -5,58 +5,87 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 /**
  * Created by atanas on 24/03/2016.
  */
 public class DriverFactory implements IWebDriver, IDesiredCapabilities {
 
-    private static final Logger logger = LoggerFactory.getLogger(IWebDriver.class);
+    private static final Logger logger = LoggerFactory.getLogger(DriverFactory.class);
 
-    private static Map<String, WebDriverThread> driverThreadPool = Collections.synchronizedMap(new LinkedHashMap<>());
-    private static ThreadLocal<WebDriverThread> driverThread = new ThreadLocal<WebDriverThread>() {
-        @Override
-        protected WebDriverThread initialValue() {
+    public void setSelectedBrowser(BrowserConfig selectedBrowser) {
+        this.selectedBrowser = selectedBrowser;
+    }
 
-            WebDriverThread webDriverThread = new WebDriverThread();
+    private BrowserConfig selectedBrowser;
 
-            if (sessionName != null)
-                driverThreadPool.put(sessionName, webDriverThread);
-            else
-                driverThreadPool.put("default", webDriverThread);
-            logger.debug("INITIAL VALUE!");
-            return webDriverThread;
-        }
-    };
+    public DriverFactory(BrowserConfig selectedBrowser) {
+        this.selectedBrowser = selectedBrowser;
+    }
 
-    private static String sessionName;
+    public DriverFactory() {
+        selectedBrowser = JVMArgs.getBrowserType();
+    }
+
+    //    private static Map<String, WebDriverThread> driverThreadPool = Collections.synchronizedMap(new LinkedHashMap<>());
+//    private static ThreadLocal<WebDriverThread> driverThread = new ThreadLocal<WebDriverThread>() {
+//        @Override
+//        protected WebDriverThread initialValue() {
+//
+//            WebDriverThread webDriverThread = new WebDriverThread();
+//
+//            if (sessionName != null)
+//                driverThreadPool.put(sessionName, webDriverThread);
+//            else
+//                driverThreadPool.put("default", webDriverThread);
+//            logger.debug("INITIAL VALUE!");
+//            return webDriverThread;
+//        }
+//    };
+
+//    private static String sessionName;
+
+    public WebDriver getDriver() {
+
+        DesiredCapabilities desiredCapabilities = selectedBrowser.getDesiredCapabilities();
+
+        return selectedBrowser.getWebDriverObject(desiredCapabilities);
+    }
+
 
     //////////////
     // SETTERS //
     /////////////
 
-    public void setSessionName(String name) {
-        logger.debug("Setting session name to: " + name);
-        sessionName = name;
-    }
+//    public void setSessionName(String name) {
+//        logger.debug("Setting session name to: " + name);
+//        sessionName = name;
+//    }
 
-    public WebDriver getDriver() {
-        return driverThread.get().getDriver();
-    }
+//    public WebDriver getDriver() {
+//        return driverThread.get().getDriver();
+//    }
+
+//    private class WebDriverThread {
+//
+//        private final BrowserConfig selectedBrowser;
+//
+//        WebDriverThread(BrowserConfig selectedBrowser) {
+//            this.selectedBrowser = selectedBrowser;
+//        }
+//
+//        WebDriverThread() {
+//            selectedBrowser = JVMArgs.getBrowserType();
+//        }
+//
+//        WebDriver getDriver() {
+//
+//            DesiredCapabilities desiredCapabilities = selectedBrowser.getDesiredCapabilities();
+//
+//            return selectedBrowser.getWebDriverObject(desiredCapabilities);
+//        }
+//
+//    }
 }
 
-class WebDriverThread {
 
-    private BrowserConfig selectedBrowser;
-
-    public WebDriver getDriver() {
-        selectedBrowser = JVMArgs.getBrowserType();
-        DesiredCapabilities desiredCapabilities = selectedBrowser.getDesiredCapabilities();
-        return selectedBrowser.getWebDriverObject(desiredCapabilities);
-    }
-
-}
 
