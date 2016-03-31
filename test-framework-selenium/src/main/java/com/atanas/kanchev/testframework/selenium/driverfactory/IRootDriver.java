3ed1c4e91bac76b0689d488dabfcff529871f3ca
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-03-31 16:50:54 +0100

    Made public

--- test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/driverfactory/IRootDriver.java
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/driverfactory/IRootDriver.java
@@ -11,7 +11,7 @@ import java.util.Properties;
 /**
  * Root Driver Interface
  */
-interface IRootDriver {
+public interface IRootDriver {
 
     // the logger
     Logger logger = LoggerFactory.getLogger(IRootDriver.class);
