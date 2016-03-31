b6740282623755ad8ad7e1345eb7320162d12cd5
Atanas Stoychev Kanchev <kanchev.atanas@gmail.com>
2016-03-31 21:16:57 +0100

    New interface IDriver

---
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/handlers/IDriver.java
@@ -0,0 +1,28 @@
+package com.atanas.kanchev.testframework.selenium.handlers;
+
+import com.atanas.kanchev.testframework.selenium.context.AbstractContext;
+import com.atanas.kanchev.testframework.selenium.context.WebContext;
+import com.atanas.kanchev.testframework.selenium.driverfactory.DriverFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by atanaskanchev on 31/03/2016.
+ */
+public interface IDriver {
+
+    // the logger
+    Logger logger = LoggerFactory.getLogger(IDriver.class);
+
+    default DriverFactory getDriverFactory(){
+        return new DriverFactory();
+    }
+
+    default void configureContext(DriverFactory driverFactory){
+        AbstractContext context = new WebContext();
+        ((WebContext) context).setDriver(driverFactory.getDriver());
+        context.addContext(context);
+    }
+
+
+}
