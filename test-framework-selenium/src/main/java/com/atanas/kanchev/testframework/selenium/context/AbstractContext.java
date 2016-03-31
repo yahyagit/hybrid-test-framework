bdd87f488b40044132cb4c1eef5adafde42b3163
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-03-31 09:40:44 +0100

    Added getter & setter for contextName

--- test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/context/AbstractContext.java
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/context/AbstractContext.java
@@ -5,9 +5,19 @@ package com.atanas.kanchev.testframework.selenium.context;
  */
 public abstract class AbstractContext<T> extends ContextFactory {
 
+    private String contextName = "context";
+
     T driver;
 
     protected abstract T getDriver();
 
     protected abstract void setDriver(T driver);
+
+    protected String getContextName() {
+        return contextName;
+    }
+
+    protected void setContextName(String contextName) {
+        this.contextName = contextName;
+    }
 }
