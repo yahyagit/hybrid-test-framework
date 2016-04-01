4b0537d360671f43424162a1091a65c7628aa028
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-04-01 15:23:10 +0100

    Added handlers refs

--- test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/handlers/IWebHandler.java
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/handlers/IWebHandler.java
@@ -3,7 +3,7 @@ package com.atanas.kanchev.testframework.selenium.handlers;
 /**
  * Web Handler Interface
  */
-public interface IWebHandler extends IBaseHandler{
+public interface IWebHandler extends IBaseHandler {
 
     default Wait waitFor() {
         return new Wait();
@@ -13,16 +13,27 @@ public interface IWebHandler extends IBaseHandler{
         return new GoTo();
     }
 
-    default Driver setup(){return new Driver();}
+    default Driver setup() {
+        return new Driver();
+    }
+
+    default Probe probe() {
+        return new Probe();
+    }
 
 
-    class Wait implements IWait {
+    class Wait implements IWait, INavigate {
     }
 
     class GoTo implements INavigate {
     }
 
-    class Driver implements IDriver{}
+    class Driver implements IDriver {
+    }
+
+    class Probe implements IProbeElement {
+    }
+
 
 }
 
