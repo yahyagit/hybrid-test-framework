390b3d1eee6629a2f3d58fd99a44d56368095add
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-09-29 11:38:00 +0100

    fixed method names

--- test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/element/interactions/element/GetCssValue.java
+++ test-framework-selenium/src/main/java/com/atanas/kanchev/testframework/selenium/element/interactions/element/GetCssValue.java
@@ -1,14 +1,17 @@
 /*
  * Copyright 2016 Atanas Stoychev Kanchev
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 
 package com.atanas.kanchev.testframework.selenium.element.interactions.element;
@@ -28,7 +31,7 @@ public class GetCssValue extends AbstractElementInteraction {
 
     private String result;
 
-    public String getAttribute(String propertyName) {
+    public String getCssValue(String propertyName) {
         try {
             return result = element.getCssValue(propertyName);
         } catch (WebDriverException e) {
