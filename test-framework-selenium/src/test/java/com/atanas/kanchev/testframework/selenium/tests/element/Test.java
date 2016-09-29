349840ff68b92b862c1fa4f13ef51d47192de43c
Atanas Kanchev <atanas.kanchev@williamhill.com>
2016-09-29 11:55:12 +0100

    externilised test

---
+++ test-framework-selenium/src/test/java/com/atanas/kanchev/testframework/selenium/tests/element/Test.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2016 Atanas Stoychev Kanchev
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.atanas.kanchev.testframework.selenium.tests.element;
+
+import com.atanas.kanchev.testframework.commons.context.ContextKey;
+import com.atanas.kanchev.testframework.selenium.accessors.SeleniumAccessorsSingleton;
+import com.atanas.kanchev.testframework.selenium.context.SeleniumContext;
+import com.atanas.kanchev.testframework.selenium.element.OmniaWebElement;
+import com.atanas.kanchev.testframework.selenium.element.proxy.ElementProxy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import static com.atanas.kanchev.testframework.commons.accessors.ContextsAccessor.$context;
+import static com.atanas.kanchev.testframework.selenium.accessors.SeleniumAccessors.$selenium;
+
+/**
+ * Created by atanas on 29/09/16.
+ */
+public class Test {
+
+    static ContextKey<SeleniumContext> currentContextKey;
+
+    public static void main(String[] args) {
+
+        $selenium().conf().setBrowser("chrome");
+        SeleniumContext<WebDriver> c = new SeleniumContext<>($selenium().conf().getDriver());
+        currentContextKey = $context().addContext(c.getContextKey(), c);
+        SeleniumAccessorsSingleton.currentContextKey = currentContextKey;
+        $selenium().goTo("https://www.google.co.uk");
+
+        OmniaWebElement wrapper = ElementProxy.wrap(OmniaWebElement.class, currentContextKey);
+        wrapper.findElement(By.className("gsfi"));
+        //        System.out.println(wrapper.isDisplayed());
+        //        wrapper.click();
+        //        wrapper.doubleClick();
+        //        wrapper.waits().titleContains("google");
+        wrapper.sendKeys("hello");
+
+    }
+}
